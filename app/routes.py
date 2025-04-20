6cf5879dbab9b3d06a148e3f7bb49c67f0955f3d
Muhammad Mughees Amjad <mugheesamjad665@gmail.com>
2025-04-20 22:05:09 +0500

    Updates

--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,31 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Task
-from . import db
 
 main = Blueprint('main', __name__)
+tasks = []
+task_id_counter = 1
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
-    tasks = Task.query.all()
+    global task_id_counter
+    if request.method == 'POST':
+        task_text = request.form.get('task')
+        if task_text:
+            tasks.append({'id': task_id_counter, 'text': task_text})
+            task_id_counter += 1
+        return redirect(url_for('main.index'))
     return render_template('index.html', tasks=tasks)
 
-@main.route('/add', methods=['POST'])
-def add():
-    title = request.form.get('title')
-    new_task = Task(title=title)
-    db.session.add(new_task)
-    db.session.commit()
+@main.route('/delete/<int:task_id>')
+def delete_task(task_id):
+    global tasks
+    tasks = [task for task in tasks if task['id'] != task_id]
     return redirect(url_for('main.index'))
 
-@main.route('/delete/<int:id>')
-def delete(id):
-    task = Task.query.get_or_404(id)
-    db.session.delete(task)
-    db.session.commit()
+@main.route('/edit/<int:task_id>', methods=['POST'])
+def edit_task(task_id):
+    new_text = request.form.get('new_task_text')
+    for task in tasks:
+        if task['id'] == task_id:
+            task['text'] = new_text
+            break
     return redirect(url_for('main.index'))
